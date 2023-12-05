6e6539d90b39be1f56254faee1cc68c1c50de4d8
Harsh Karan <karanharshe577@gmail.com>
2023-12-05 19:21:36 +0530

    create sign api route

--- api/index.js
+++ api/index.js
@@ -2,8 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
 
 dotenv.config()
+
+
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("DataBase connected");
 })
@@ -13,8 +16,10 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 const app = express();
 
+app.use(express.json());
 app.listen(3000 , ()=>{
     console.log("Server started on port 3000");
 });
 
 app.use("/api/user" , userRoute)
+app.use("/api/auth" , authRoute)
